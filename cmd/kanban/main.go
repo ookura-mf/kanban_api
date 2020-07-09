3c0b7dd0218aae26a16519d55066a9dc85f6d54b
Ookura Keisuke <ookura.keisuke@moneyforward.co.jp>
2020-07-09 23:26:01 +0900

    dbの初期化処理を追加ika

--- cmd/kanban/main.go
+++ cmd/kanban/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
+
+	db, err := gorm.Open("mysql", "root:@/kanban_development?parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"health": "ok"})
