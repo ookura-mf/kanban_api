827c897265e731a655c964f0ddf6f26d98df1142
Ookura Keisuke <ookura.keisuke@moneyforward.co.jp>
2020-07-12 22:48:19 +0900

    add action for creating kanban

---
+++ internal/controllers/kanban_controller.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"net/http"
+	"github.com/kanban_api/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo"
+)
+
+func CreateKanban(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		kanban := new(models.Kanban)
+		if err := c.Bind(kanban); err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"messages": "something wrong!"})
+		}
+		db.Create(&kanban)
+	
+		return c.JSON(http.StatusOK, kanban)
+	}
+}
