a5279c1750402f17b604037d74524621b78331d8
Ookura Keisuke <ookura.keisuke@moneyforward.co.jp>
2020-07-12 22:48:47 +0900

    add route for creating kanban.

--- cmd/kanban/main.go
+++ cmd/kanban/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"net/http"
-
+	"github.com/kanban_api/internal/controllers"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
@@ -17,9 +17,11 @@ func main() {
 	}
 	defer db.Close()
 
+	// Routing
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"health": "ok"})
 	})
+	e.POST("/kanbans", controllers.CreateKanban(db))
 
 	e.Logger.Fatal(e.Start(":1313"))
 }
