7f0c08647daa8158f4c40e50ba19155f6fe346c0
Ookura Keisuke <ookura.keisuke@moneyforward.co.jp>
2020-07-13 23:15:41 +0900

    テーブル定義の修正

--- db/migrations/20200704233549_add_task_table.sql
+++ db/migrations/20200704233549_add_task_table.sql
@@ -2,14 +2,15 @@
 -- SQL in this section is executed when the migration is applied.
 CREATE TABLE tasks (
     id int NOT NULL AUTO_INCREMENT,
-    kanbanId int NOT NULL,
+    kanban_id int NOT NULL,
     title varchar(255) NOT NULL,
     content text,
-    status int NOT NULL,
-    createdAt datetime,
-    updatedAt datetime,
-    deletedAt datetime,
-    PRIMARY KEY(id)
+    status int NOT NULL DEFAULT 1 COMMENT '1: todo, 2: doing, 3: done',
+    created_at datetime,
+    updated_at datetime,
+    deleted_at datetime,
+    PRIMARY KEY(id),
+    FOREIGN KEY (kanban_id) REFERENCES kanbans(id) ON DELETE CASCADE
 );
 
 -- +goose Down
